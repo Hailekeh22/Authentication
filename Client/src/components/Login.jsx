b798f5b2572d8f58b0e2ea9f8656fb4658837ff8
Hailekeh22 <hailekjt@gmail.com>
2024-07-24 20:24:54 +0300

    input recived and axios setup

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
+  const handlechanges = (e) => {
+    const { name, value } = e.target;
+
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
+
   return (
     <div className=" h-screen w-full bg-slate-800 flex items-center justify-center">
       <div className=" p-4 bg-slate-400 rounded-lg w-80">
-        <form action="" method="POST" className=" flex flex-col w-full">
+        <div className="flex items-center justify-center ">
+          <h2 className=" font-bold">Login</h2>
+        </div>
+        <form onSubmit={login} className=" flex flex-col w-full">
           <label>Email</label>
-          <input type="email" className=" rounded-md p-1"></input>
+          <input
+            type="email"
+            className=" rounded-md p-1"
+            name="email"
+            onChange={handlechanges}
+          ></input>
           <label>password</label>
-          <input type="password" className=" rounded-md p-1"></input>
+          <input
+            type="password"
+            className=" rounded-md p-1"
+            name="password"
+            onChange={handlechanges}
+          ></input>
           <button type="submit" className=" primary-btn mt-3">
             Login
           </button>
--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const [inputValues, setInputValues] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handlechange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
+  const signup = (e) => {};
+
   return (
     <div className=" h-screen w-full bg-slate-800 flex items-center justify-center">
       <div className=" p-4 bg-slate-400 rounded-lg w-80">
-        <form action="" method="POST" className=" flex flex-col w-full">
+        <div className="flex items-center justify-center ">
+          <h2 className=" font-bold">Sign Up</h2>
+        </div>
+        <form onSubmit={signup} className=" flex flex-col w-full">
           <label>First Name</label>
-          <input type="text" className=" rounded-md p-1"></input>
+          <input
+            type="text"
+            className=" rounded-md p-1"
+            name="firstname"
+            onChange={handlechange}
+          ></input>
           <label>Last Name</label>
-          <input type="text" className=" rounded-md p-1"></input>
+          <input
+            type="text"
+            className=" rounded-md p-1"
+            name="lastname"
+            onChange={handlechange}
+          ></input>
           <label>Email</label>
-          <input type="email" className=" rounded-md p-1"></input>
+          <input
+            type="email"
+            className=" rounded-md p-1"
+            name="email"
+            onChange={handlechange}
+          ></input>
           <label>password</label>
-          <input type="password" className=" rounded-md p-1"></input>
+          <input
+            type="password"
+            className=" rounded-md p-1"
+            name="password"
+            onChange={handlechange}
+          ></input>
           <button type="submit" className=" primary-btn mt-3">
             Sign in
           </button>
